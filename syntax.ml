262b27db8b38f6b91b026358808596864f4c4061
Matteo Busi <matteo.busi@di.unipi.it>
2020-07-31 14:12:53 +0200

    Small changes; Added .gitignore

--- syntax.ml
+++ syntax.ml
@@ -41,61 +41,52 @@ let rec string_of_type (t : Type.t) =
     | Type.Int -> Printf.sprintf "int"
     | Type.Bool -> Printf.sprintf "bool"
     | Type.Float -> Printf.sprintf "float"
-    | Type.Array(t) -> Printf.sprintf "[%s]" (string_of_type t)
+    | Type.Array(t) ->
+      Printf.sprintf "(%s array)" (string_of_type t)
+      (* Printf.sprintf "[%s]" (string_of_type t) *)
     | Type.Tuple(ts) -> string_of_tuplet ts
     | Type.Fun(args, rt) -> Printf.sprintf "%s -> %s" (string_of_tuplet args) (string_of_type rt)
     | Type.Var ({ contents = None }) -> failwith "Error: should never happen"
     | Type.Var ({ contents = Some t }) -> Printf.sprintf "Var %s" (string_of_type t)
 
 let rec string_of_e (e : t) =
-  let rec ntabber n =
-    if n = 0 then "" else "    " ^ ntabber (n - 1) in
-  let rec _string_of_e (e : t) (d : int) =
-    let string_of_tuple ts =
-      let tss = List.mapi
-        (fun i t -> if i = 0 then Printf.sprintf "%s" (_string_of_e t (d+1)) else Printf.sprintf ", %s" (_string_of_e t (d+1))) ts in
-      Printf.sprintf "(%s)" (List.fold_left (fun (a : string) ts -> Printf.sprintf "%s%s" a ts) "" tss) in
-    let string_of_args ts = List.fold_left (fun (a : string) t -> Printf.sprintf "%s %s" a (_string_of_e t (d+1))) "" ts in
-    let string_of_id x = if String.get x 0 = 'T' then "_" else x in
-    match e with
-      | Unit ->  "()"
-      | Bool t -> Printf.sprintf "%b" t
-      | Int i -> Printf.sprintf "%d" i
-      | Float f -> Printf.sprintf "%f" f
-      | Not e1 -> Printf.sprintf "(not %s)" (_string_of_e e1 (d+1))
-      | Neg e1 -> Printf.sprintf "(- %s)" (_string_of_e e1 (d+1))
-      | Add (e1, e2) -> Printf.sprintf "(%s + %s)" (_string_of_e e1 (d+1)) (_string_of_e e2 (d+1))
-      | Sub (e1, e2) -> Printf.sprintf "(%s - %s)" (_string_of_e e1 (d+1)) (_string_of_e e2 (d+1))
-      | FNeg e1 -> Printf.sprintf "(-. %s)" (_string_of_e e1 (d+1))
-      | FAdd (e1, e2) -> Printf.sprintf "(%s +. %s)" (_string_of_e e1 (d+1)) (_string_of_e e2 (d+1))
-      | FSub (e1, e2) -> Printf.sprintf "(%s -. %s)" (_string_of_e e1 (d+1)) (_string_of_e e2 (d+1))
-      | FMul (e1, e2) -> Printf.sprintf "(%s *. %s)" (_string_of_e e1 (d+1)) (_string_of_e e2 (d+1))
-      | FDiv (e1, e2) -> Printf.sprintf "(%s /. %s)" (_string_of_e e1 (d+1)) (_string_of_e e2 (d+1))
-      | Eq (e1, e2) -> Printf.sprintf "(%s = %s)" (_string_of_e e1 (d+1)) (_string_of_e e2 (d+1))
-      | LE (e1, e2) -> Printf.sprintf "(%s <= %s)" (_string_of_e e1 (d+1)) (_string_of_e e2 (d+1))
-      | If (e1, e2, e3) -> Printf.sprintf "(if %s then %s else %s)" (_string_of_e e1 (d+1)) (_string_of_e e2 (d+1)) (_string_of_e e3 (d+1))
-      | Let ((x, tx), e2, e3) ->
-        Printf.sprintf "(let %s = %s in\n%s%s)" (string_of_id x) (_string_of_e e2 (d+1))
-        (* Printf.sprintf "(let %s : %s = %s in\n%s%s)" (string_of_id x) (string_of_type tx) (_string_of_e e2 (d+1)) *)
-        (ntabber d) (_string_of_e e3 (d+1))
-      | Var x -> string_of_id x
-      | App (e, el) -> Printf.sprintf "(%s %s)" (_string_of_e e (d+1)) (string_of_args el)
-      | Tuple el -> Printf.sprintf "%s" (string_of_tuple el)
-      | LetTuple (xtl, e1, e2) ->
-        let xl = List.map (fun xt -> Var (fst xt)) xtl in
-        let tl = List.map snd xtl in
-          Printf.sprintf "(let %s = %s in %s)" (string_of_tuple xl) (_string_of_e e1 (d+1)) (_string_of_e e2 (d+1))
-          (* Printf.sprintf "(let %s : %s = %s in %s)" (string_of_tuple xl) (string_of_type (Type.Tuple tl)) (_string_of_e e1 (d+1)) (_string_of_e e2 (d+1)) *)
-      | Array (e1, e2) -> Printf.sprintf "(Array.make %s %s)" (_string_of_e e1 (d+1)) (_string_of_e e2 (d+1))
-      | Get (e1, e2) -> Printf.sprintf "(%s.(%s))" (_string_of_e e1 (d+1)) (_string_of_e e2 (d+1))
-      | Put (e1, e2, e3) -> Printf.sprintf "(%s.(%s) <- %s)" (_string_of_e e1 (d+1)) (_string_of_e e2 (d+1)) (_string_of_e e3 (d+1))
-      | LetRec ({ name = (f, tf); args = al; body = e1}, e2) ->
-        match tf with
-          | Type.Fun(fargs, frt) ->
-              let sal = List.fold_left (fun a (x, t) -> Printf.sprintf "%s (%s : %s)" a (string_of_id x) (string_of_type t)) "" al in
-              if d = 0 then
-                Printf.sprintf "let rec %s%s : %s = %s in\n%s%s" f sal (string_of_type frt) (_string_of_e e1 (d+1)) (ntabber d) (_string_of_e e2 (d+1))
-              else
-                Printf.sprintf "(let rec %s%s : %s = %s in\n%s%s)" f sal (string_of_type frt) (_string_of_e e1 (d+1)) (ntabber d) (_string_of_e e2 (d+1))
-          | _ -> failwith "Never happens."
-  in _string_of_e e 0
+  let string_of_tuple ts =
+    let tss = List.mapi
+      (fun i t -> if i = 0 then Printf.sprintf "%s" (string_of_e t) else Printf.sprintf ", %s" (string_of_e t)) ts in
+    Printf.sprintf "(%s)" (List.fold_left (fun (a : string) ts -> Printf.sprintf "%s%s" a ts) "" tss) in
+  let string_of_args ts = List.fold_left (fun (a : string) t -> Printf.sprintf "%s %s" a (string_of_e t)) "" ts in
+  let string_of_id x = if String.get x 0 = 'T' then "_" else x in
+  match e with
+    | Unit ->  "()"
+    | Bool t -> Printf.sprintf "%b" t
+    | Int i -> Printf.sprintf "%d" i
+    | Float f -> Printf.sprintf "%f" f
+    | Not e1 -> Printf.sprintf "(not %s)" (string_of_e e1)
+    | Neg e1 -> Printf.sprintf "(- %s)" (string_of_e e1)
+    | Add (e1, e2) -> Printf.sprintf "(%s + %s)" (string_of_e e1) (string_of_e e2)
+    | Sub (e1, e2) -> Printf.sprintf "(%s - %s)" (string_of_e e1) (string_of_e e2)
+    | FNeg e1 -> Printf.sprintf "(-. %s)" (string_of_e e1)
+    | FAdd (e1, e2) -> Printf.sprintf "(%s +. %s)" (string_of_e e1) (string_of_e e2)
+    | FSub (e1, e2) -> Printf.sprintf "(%s -. %s)" (string_of_e e1) (string_of_e e2)
+    | FMul (e1, e2) -> Printf.sprintf "(%s *. %s)" (string_of_e e1) (string_of_e e2)
+    | FDiv (e1, e2) -> Printf.sprintf "(%s /. %s)" (string_of_e e1) (string_of_e e2)
+    | Eq (e1, e2) -> Printf.sprintf "(%s = %s)" (string_of_e e1) (string_of_e e2)
+    | LE (e1, e2) -> Printf.sprintf "(%s <= %s)" (string_of_e e1) (string_of_e e2)
+    | If (e1, e2, e3) -> Printf.sprintf "(if %s then %s else %s)" (string_of_e e1) (string_of_e e2) (string_of_e e3)
+    | Let ((x, tx), e2, e3) ->
+      Printf.sprintf "(let %s = %s in %s)" (string_of_id x) (string_of_e e2) (string_of_e e3)
+    | Var x -> string_of_id x
+    | App (e, el) -> Printf.sprintf "(%s %s)" (string_of_e e) (string_of_args el)
+    | Tuple el -> Printf.sprintf "%s" (string_of_tuple el)
+    | LetTuple (xtl, e1, e2) ->
+      let xl = List.map (fun xt -> Var (fst xt)) xtl in
+      let tl = List.map snd xtl in
+        Printf.sprintf "(let %s = %s in %s)" (string_of_tuple xl) (string_of_e e1) (string_of_e e2)
+    | Array (e1, e2) -> Printf.sprintf "(Array.make %s %s)" (string_of_e e1) (string_of_e e2)
+    | Get (e1, e2) -> Printf.sprintf "(%s.(%s))" (string_of_e e1) (string_of_e e2)
+    | Put (e1, e2, e3) -> Printf.sprintf "(%s.(%s) <- %s)" (string_of_e e1) (string_of_e e2) (string_of_e e3)
+    | LetRec ({ name = (f, tf); args = al; body = e1}, e2) ->
+      match tf with
+        | Type.Fun(fargs, frt) ->
+            let sal = List.fold_left (fun a (x, t) -> Printf.sprintf "%s (%s : %s)" a (string_of_id x) (string_of_type t)) "" al in Printf.sprintf "(let rec %s%s : %s = %s in %s)" f sal (string_of_type frt) (string_of_e e1) (string_of_e e2)
+        | _ -> failwith "Never happens."
